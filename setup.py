2251e491206c3c27fa61daf82683594383779d88
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2021-08-09 16:25:16 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="rca-trackhome",
+    version="0.0.1",
+    description="Home user tracking project",
+    author="Robin Roussel",
+    author_email="robin.roussel@rca.ac.uk",
+    url="https://github.com/rrssl/rca-trackhome",
+    packages=["trkpy"],
+)
