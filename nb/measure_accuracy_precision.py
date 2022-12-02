80476d6c02905b2e624363b95d16f9bfe6c4cf03
Robin Roussel <robin.roussel@rca.ac.uk>
2022-12-02 16:13:09 +0000

    Process experiment points in a loop

--- nb/measure_accuracy_precision.py
+++ nb/measure_accuracy_precision.py
@@ -38,32 +38,57 @@ ACONF = {
     'device': "rpi1",
     'tag': "0x7625",
     'experiment': "accuracy-1",
-    # 'points': {
-    #     'A': (1635, 1730),
-    #     'B': (1190, 6580-2100),
-    #     'C': (3020, 7130-600)
-    # },
-    # 'display': {
-    #     'x': 2300,
-    #     'y': 2550,
-    #     'scale': 9500,
-    #     'rotation': 0
-    # }
-    'points': {
+    'locations': {
+        # upper/accuracy-1
+        # 'A': (1635, 1730),
+        # 'B': (1190, 6580-2100),
+        # 'C': (3020, 7130-600)
+        # upper/accuracy-2
+        # 'A': (4652, 661),
+        # 'B': (370, 1191),
+        # 'C': (3413, 3083),
+        # 'D': (1132, 4739)
+        # lower/accuracy-1
         'A': (1290, 7130-810),
         'B': (1670, 4817-1393+385),
         'C': (2780-346, 4817-1393-340),
         'D': (994, 486),
         'E': (4745-480, 7130-2208+1365),
-        'F': (2780+130, 4817-1393),
+        'F': (2780+130, 4817-1393)
     },
-    # 'display': {
-    #     'x': 2300,
-    #     'y': 2550,
-    #     'scale': 9500,
-    #     'rotation': 0
-    # }
+    'day': 30,
+    'month': 11,
+    'times': {
+        # upper/accuracy-1
+        # 'A': ('19:50', '20:20'),
+        # 'B': ('18:50', '19:20'),
+        # 'C': ('20:50', '21:20')
+        # upper/accuracy-2
+        # 'A': ('12:15', '14:45'),
+        # 'B': ('15:10', '17:40'),
+        # 'C': ('18:00', '20:30'),
+        # 'D': ('09:00', '11:30')
+        # 'A': ('11:45', '14:15'),
+        # 'B': ('14:45', '17:15'),
+        # 'C': ('17:45', '20:15')
+        # 'A': ('13:45', '17:15'),
+        # 'B': ('17:45', '20:15'),
+        # 'D': ('09:15', '13:15')
+        # lower/accuracy-1
+        'A': ('09:05', '10:55'),
+        'B': ('11:05', '12:55'),
+        'C': ('13:05', '14:55'),
+        'D': ('15:05', '16:55'),
+        'E': ('17:05', '20:25'),
+        'F': ('21:15', '22:35')
+    },
     'display': {
+        # upper/accuracy-2
+        # 'x': 2300,
+        # 'y': 2550,
+        # 'scale': 9500,
+        # 'rotation': 0
+        # lower/accuracy-1
         'x': 2375,
         'y': 3550,
         'scale': 7450,
@@ -93,17 +118,11 @@ timestamps = pd.to_datetime(data['t'], unit='ms', utc=True).dt.tz_convert("Europ
 data = data.set_index(timestamps).sort_index()
 data = data.drop(data[(data['x'] == 0) & (data['y'] == 0)].index)
 data = data.loc[~((data['x'].shift(-1) == data['x']) & (data['y'].shift(-1) == data['y']))]
-data = data[(data.index.month == 11) & (data.index.day == 30)]
-# data = data.between_time('18:00', '22:00')
-# data_A = data.between_time('19:50', '20:20')
-# data_B = data.between_time('18:50', '19:20')
-# data_C = data.between_time('20:50', '21:20')
-data_A = data.between_time('09:05', '10:55')
-data_B = data.between_time('11:05', '12:55')
-data_C = data.between_time('13:05', '14:55')
-data_D = data.between_time('15:05', '16:55')
-data_E = data.between_time('17:05', '20:25')
-data_F = data.between_time('21:15', '22:35')
+data = data[(data.index.month == conf['month']) & (data.index.day == conf['day'])]
+points_data = {
+    point: data.between_time(start, end)
+    for point, (start, end) in conf['times'].items()
+}
 
 floorplan_img = Image.open(floorplan_path)
 if conf['display']['rotation']:
@@ -126,12 +145,13 @@ for name, xyz in anchors.items():
                 path_effects=[pe.withStroke(linewidth=2, foreground='w')])
 ax.imshow(np.asarray(floorplan_img), extent=extent.ravel(), zorder=2)
 
-for point in "ABCDEF":
-    ax.scatter(*conf['points'][point], facecolor='black', edgecolor='white', s=50, zorder=6)
-    ax.annotate(point, conf['points'][point], xytext=(5, 5), textcoords='offset pixels',
-                path_effects=[pe.withStroke(linewidth=2, foreground='w')], zorder=6)
-for data_i in (data_A, data_B, data_C, data_D, data_E, data_F):
-    ax.scatter(data_i['x'], data_i['y'], alpha=.3, edgecolor='none', s=25, zorder=5)
+for point, location in conf['locations'].items():
+    ax.scatter(*location, facecolor='black', edgecolor='white', s=50, zorder=6)
+    ax.annotate(point, location, xytext=(5, 5), textcoords='offset pixels',
+                path_effects=[pe.withStroke(linewidth=2, foreground='w')],
+                zorder=6)
+    ax.scatter(points_data[point]['x'], points_data[point]['y'], alpha=.3,
+               edgecolor='none', s=25, zorder=5)
 
 # -
 
@@ -153,12 +173,10 @@ def compute_kde(data, res, margin):
 
 res = 100
 margin = 500
-density_A, extent_A = compute_kde(data_A, res, margin)
-density_B, extent_B = compute_kde(data_B, res, margin)
-density_C, extent_C = compute_kde(data_C, res, margin)
-density_D, extent_D = compute_kde(data_D, res, margin)
-density_E, extent_E = compute_kde(data_E, res, margin)
-density_F, extent_F = compute_kde(data_F, res, margin)
+density_extent_kde = {
+    point: compute_kde(point_data, res, margin)
+    for point, point_data in points_data.items()
+}
 
 
 # +
@@ -170,17 +188,13 @@ ax.scatter(*zip(*[xyz[:2] for xyz in anchors.values()]), marker='s', zorder=3)
 for name, xyz in anchors.items():
     ax.annotate(name, xyz[:2], xytext=(5, 5), textcoords='offset pixels',
                 path_effects=[pe.withStroke(linewidth=2, foreground='w')])
-for point in "ABCDEF":
-    ax.scatter(*conf['points'][point], facecolor='black', edgecolor='white', s=50, zorder=6)
-    ax.annotate(point, conf['points'][point], xytext=(5, 5), textcoords='offset pixels',
+for point, location in conf['locations'].items():
+    ax.scatter(*location, facecolor='black', edgecolor='white', s=50, zorder=6)
+    ax.annotate(point, location, xytext=(5, 5), textcoords='offset pixels',
                 path_effects=[pe.withStroke(linewidth=2, foreground='w')])
-
-ax.imshow(density_A, cmap=plt.cm.gist_earth_r, extent=extent_A, alpha=0.5)
-ax.imshow(density_B, cmap=plt.cm.gist_earth_r, extent=extent_B, alpha=0.5)
-ax.imshow(density_C, cmap=plt.cm.gist_earth_r, extent=extent_C, alpha=0.5)
-ax.imshow(density_D, cmap=plt.cm.gist_earth_r, extent=extent_D, alpha=0.5)
-ax.imshow(density_E, cmap=plt.cm.gist_earth_r, extent=extent_E, alpha=0.5)
-ax.imshow(density_F, cmap=plt.cm.gist_earth_r, extent=extent_F, alpha=0.5)
+for point, (point_density, point_extent) in density_extent_kde.items():
+    ax.imshow(point_density, cmap=plt.cm.gist_earth_r, extent=point_extent,
+              alpha=.5)
 ax.imshow(np.asarray(floorplan_img), extent=extent.ravel(), zorder=2)
 # ax.plot(values[0], values[1], 'k.', markersize=2)
 # ax.set_xlim([xmin, xmax])
@@ -189,30 +203,18 @@ ax.imshow(np.asarray(floorplan_img), extent=extent.ravel(), zorder=2)
 
 agg_data = {
     'Accuracy (mm)': [
-        np.linalg.norm(data_A[['x', 'y']].mean() - conf['points']['A']),
-        np.linalg.norm(data_B[['x', 'y']].mean() - conf['points']['B']),
-        np.linalg.norm(data_C[['x', 'y']].mean() - conf['points']['C']),
-        np.linalg.norm(data_D[['x', 'y']].mean() - conf['points']['D']),
-        np.linalg.norm(data_E[['x', 'y']].mean() - conf['points']['E']),
-        np.linalg.norm(data_F[['x', 'y']].mean() - conf['points']['F']),
+        np.linalg.norm(point_data[['x', 'y']].mean() - conf['locations'][point])
+        for point, point_data in points_data.items()
     ],
     'Std X (mm)': [
-        data_A['x'].std(),
-        data_B['x'].std(),
-        data_C['x'].std(),
-        data_D['x'].std(),
-        data_E['x'].std(),
-        data_F['x'].std(),
+        point_data['x'].std()
+        for _, point_data in points_data.items()
     ],
     'Std Y (mm)': [
-        data_A['y'].std(),
-        data_B['y'].std(),
-        data_C['y'].std(),
-        data_D['y'].std(),
-        data_E['y'].std(),
-        data_F['y'].std(),
+        point_data['y'].std()
+        for _, point_data in points_data.items()
     ],
 }
-agg_data = pd.DataFrame(agg_data, index=list("ABCDEF"))
+agg_data = pd.DataFrame(agg_data, index=list(points_data.keys()))
 agg_data.loc["Mean"] = agg_data.mean()
 agg_data.astype(int)
