c304f8c6707a1336b4a81c26a33a634cb615cf81
Robin Roussel <robin.roussel@rca.ac.uk>
2023-02-10 10:59:27 +0000

    Exit HAT manager gracefully when another is already running

--- scripts/run_hat_manager.py
+++ scripts/run_hat_manager.py
@@ -25,10 +25,7 @@ def get_arg_parser():
 
 
 def init_io(dummy: bool = False):
-    if dummy:
-        hat = DummyHat()
-    else:
-        hat = JamHat()
+    hat = DummyHat() if dummy else JamHat()
     # Lights on the first row are all managed by the tracker daemon, which
     # should be spawned after the HAT manager.
     hat.lights_1.off()
@@ -50,47 +47,53 @@ def set_buttons_callback(hat: JamHat, hard_in: Connection):
     hat.button_2.when_held = b2_cb
 
 
+def run_event_loop(listener: Listener, conf: dict):
+    hat = init_io(dummy=conf['dummy'])
+    running = True
+    while running:
+        with listener.accept() as hard_out:
+            while True:
+                try:
+                    msg = hard_out.recv()
+                except EOFError:
+                    break
+                # System messages
+                if msg == "LISTENING":
+                    hard_in = Client(tuple(conf['hat']['address_in']))
+                    set_buttons_callback(hat, hard_in)
+                elif msg == "POWEROFF":
+                    running = False
+                    hard_in.close()
+                    # Blink O2 for 3 blocking seconds then exit the loop.
+                    hat.lights_2.yellow.blink(.5, .5, 3, background=False)
+                    break
+                else:
+                    # Output signals
+                    led_name, state = msg
+                    led_board = {
+                        '1': hat.lights_1,
+                        '2': hat.lights_2
+                    }[led_name[1]]
+                    led = {
+                        'R': led_board.red,
+                        'O': led_board.yellow,
+                        'G': led_board.green
+                    }[led_name[0]]
+                    if state:
+                        led.on()
+                    else:
+                        led.off()
+
+
 def main():
     track_publish.get_arg_parser = get_arg_parser
     conf = track_publish.get_config()
-    address_out = tuple(conf['hat']['address_out'])
-    address_in = tuple(conf['hat']['address_in'])
-    with Listener(address_out) as listener:
-        hat = init_io(dummy=conf['dummy'])
-        running = True
-        while running:
-            with listener.accept() as hard_out:
-                while True:
-                    try:
-                        msg = hard_out.recv()
-                    except EOFError:
-                        break
-                    # System messages
-                    if msg == "LISTENING":
-                        hard_in = Client(address_in)
-                        set_buttons_callback(hat, hard_in)
-                    elif msg == "POWEROFF":
-                        running = False
-                        hard_in.close()
-                        # Blink O2 for 3 blocking seconds then exit the loop.
-                        hat.lights_2.yellow.blink(.5, .5, 3, background=False)
-                        break
-                    else:
-                        # Output signals
-                        led_name, state = msg
-                        led_board = {
-                            '1': hat.lights_1,
-                            '2': hat.lights_2
-                        }[led_name[1]]
-                        led = {
-                            'R': led_board.red,
-                            'O': led_board.yellow,
-                            'G': led_board.green
-                        }[led_name[0]]
-                        if state:
-                            led.on()
-                        else:
-                            led.off()
+    try:
+        with Listener(tuple(conf['hat']['address_out'])) as listener:
+            run_event_loop(listener)
+    except OSError:
+        # Another HAT manager is already listening.
+        return
 
 
 if __name__ == "__main__":
