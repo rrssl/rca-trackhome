eda123b5de5c92c0dc6814082227b7abbe213146
Robin Roussel <robin.roussel@rca.ac.uk>
2024-01-30 14:58:23 +0000

    Add scrolled text area

--- scripts/test_aws_connectivity.py
+++ scripts/test_aws_connectivity.py
@@ -2,22 +2,29 @@
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 
 
-def update_process_output(window, process, scrolled_text):
+def update_process_output(window, process, text_area):
+    text_area.configure(state='normal')
     try:
-        scrolled_text['text'] += next(process.stdout).decode('utf8')
+        line = next(process.stdout).decode('utf8')
+        text_area.insert(tk.INSERT, line)
     except StopIteration:
         pass
-    window.after(10, update_process_output, window, process, scrolled_text)
+    text_area.configure(state='disabled')
+    window.after(10, update_process_output, window, process, text_area)
 
 
 def main():
     window = tk.Tk()
-    window.title("Hello World")
+    window.title("AWS Connectivity test")
 
-    scrolled_text = tk.Label(text="Hello, Tkinter\n", justify=tk.LEFT)
-    scrolled_text.pack()
+    header = tk.Label(text="This is a test")
+    header.pack()
+    scroll_txt = ScrolledText(window)
+    scroll_txt.configure(state='disabled')  # make the text read-only
+    scroll_txt.pack()
 
     # Option for the number of packets as a function of
     param = "-n" if platform.system().lower() == "windows" else "-c"
@@ -29,7 +36,7 @@ def main():
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT
     ) as process:
-        window.after(10, update_process_output, window, process, scrolled_text)
+        window.after(10, update_process_output, window, process, scroll_txt)
         window.mainloop()
 
 
