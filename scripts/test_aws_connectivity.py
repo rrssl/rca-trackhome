f615a4ed730be36766a2d06c83099e5d21b75aaa
Robin Roussel <robin.roussel@rca.ac.uk>
2024-01-30 16:58:46 +0000

    Accept test URL as input

--- scripts/test_aws_connectivity.py
+++ scripts/test_aws_connectivity.py
@@ -5,39 +5,44 @@ import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 
 
-def update_process_output(window, process, text_area):
+def append_readonly_output(text_area, text):
     text_area.configure(state='normal')
-    try:
-        line = next(process.stdout).decode('utf8')
-        text_area.insert(tk.INSERT, line)
-    except StopIteration:
-        pass
+    text_area.insert(tk.INSERT, text)
     text_area.configure(state='disabled')
-    window.after(10, update_process_output, window, process, text_area)
 
 
-def main():
-    window = tk.Tk()
-    window.title("AWS Connectivity test")
-
-    header = tk.Label(text="This is a test")
-    header.pack()
-    scroll_txt = ScrolledText(window)
-    scroll_txt.configure(state='disabled')  # make the text read-only
-    scroll_txt.pack()
-
-    # Option for the number of packets as a function of
+def run_connectivity_test(window, input_field, output_area):
     param = "-n" if platform.system().lower() == "windows" else "-c"
-    # Building the command. Ex: "ping -c 1 google.com"
-    host = "8.8.8.8"
-    command = ["ping", param, "3", host]
+    command = ["ping", param, "3", input_field.get()]
     with subprocess.Popen(
         command,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT
     ) as process:
-        window.after(10, update_process_output, window, process, scroll_txt)
-        window.mainloop()
+        for line in process.stdout:
+            append_readonly_output(output_area, line.decode())
+        # window.after(10, update_process_output, window, process, output_area)
+
+
+def main():
+    # GUI
+    window = tk.Tk()
+    window.title("AWS Connectivity test")
+    header = tk.Label(text="Enter the AWS endpoint URL:")
+    header.pack()
+    input_field = tk.Entry(window)
+    input_field.pack()
+    output_area = ScrolledText(window)
+    output_area.configure(state='disabled')  # make the text read-only
+    action_btn = tk.Button(
+        window,
+        text="Run test",
+        command=lambda: run_connectivity_test(window, input_field, output_area)
+    )
+    action_btn.pack()
+    output_area.pack()
+    # Run the GUI event loop.
+    window.mainloop()
 
 
 if __name__ == "__main__":
